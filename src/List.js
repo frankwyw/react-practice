e1fc206c137a265910b2a612bbb820ff60c35561
frankwyw <wangyaowei7@gmail>
2018-06-17 15:36:47 +0800

    Add list map in react

--- src/List.js
+++ src/List.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import ListItem from "./ListItem";
 
 
+const numbers = [1, 2, 3, 4, 5];
+
+const listItems = numbers.map((number) =>
+    <li key = {number}>{number}</li>
+);
+
 class List extends Component {
     constructor(props) {
         super(props);
@@ -29,9 +35,13 @@ class List extends Component {
                     <ol>
                         {/*<button  onClick={this.handleClick()}>  {this.state.isToggleOn ? 'ON' : 'OFF'} </button> ERROR */}
                         <button  onClick={this.handleClick}>  {this.state.isToggleOn ? 'ON' : 'OFF'} </button>
+
+                        {this.state.isToggleOn ? <li>true</li> : <li>false</li>}
                         <ListItem value = {this.state.value++}/>
                         <ListItem value = {this.state.value++}/>
                         <ListItem value = {this.state.value++}/>
+                        <br/>
+                        {listItems}
                     </ol>
                 </p>
             </div>
